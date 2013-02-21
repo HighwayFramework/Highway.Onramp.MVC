3686f2b37337e01ee7031d6d7c01c62c8c8130c6
Tim Rayburn <tim@timrayburn.net>
2013-02-20 18:30:04 -0600

    Updating rake to properly pack packages

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,16 +5,16 @@ require 'fileutils'
 
 CONFIG        = 'Debug'
 RAKE_DIR      = File.expand_path(File.dirname(__FILE__))
-SOLUTION_DIR  = RAKE_DIR + "/Highway"
+SOLUTION_DIR  = RAKE_DIR + "/src"
 TEMPLATE_DIR  = RAKE_DIR + "/Templates"
 TEST_DIR      = SOLUTION_DIR + "/test/"
-SRC_DIR       = SOLUTION_DIR + "/src/"
+SRC_DIR       = SOLUTION_DIR + "/Highway.MVC/"
 SOLUTION_FILE = 'Highway.sln'
 TEMPLATE_FILE = 'Templates.MVC.sln'
 MSTEST        = ENV['VS110COMNTOOLS'] + "..\\IDE\\mstest.exe"
 NUGET         = SOLUTION_DIR + "/.nuget/nuget.exe"
 
-task :default                     => ['build:mvc', 'build:data', 'build:logging']
+task :default                     => ['build:mvc', 'build:data', 'build:logging', 'build:all']
 task :test                        => ['build:mstest' ]
 task :package                     => ['package:packall']
 task :push                        => ['package:pushall']
@@ -30,29 +30,39 @@ namespace :build do
 	
 	task :mvc do
 		create 'build_mvc/'
-		basePath = 'src/Templates.MVC/'
 		files = [ 'App_Start/IoC.cs', 'Services/WindsorControllerFactory.cs', 'Installers/ControllerInstaller.cs', 'Installers/FilterInstaller.cs', 'Services/IoCFilterProvider.cs', 'App_Start/FilterProvidersWireup.cs', 'App_Start/ControllerFactoryWireup.cs' ]
 		files.each do |file|
-			convert_to_pp basePath, file, 'build_mvc/content/'
+			convert_to_pp SRC_DIR, file, 'build_mvc/content/'
 		end
+		FileUtils.cp(SOLUTION_DIR + '/nuspec/Highway.Onramp.MVC.nuspec', RAKE_DIR + '/build_mvc/Highway.Onramp.MVC.nuspec')
+		sh SOLUTION_DIR + '/.nuget/NuGet.exe pack ' + RAKE_DIR + '/build_mvc/Highway.Onramp.MVC.nuspec -o output'
 	end
+	
+	task :all do
+		create 'build_all/'
+		FileUtils.cp(SOLUTION_DIR + '/nuspec/Highway.Onramp.MVC.All.nuspec', RAKE_DIR + '/build_all/Highway.Onramp.MVC.All.nuspec')
+		sh SOLUTION_DIR + '/.nuget/NuGet.exe pack ' + RAKE_DIR + '/build_all/Highway.Onramp.MVC.All.nuspec -o output'
+	end
+
 		
 	task :data do
 		create 'build_data/'
-		basePath = 'src/Templates.MVC/'
 		files = [ 'App_Start/DatabaseInitializerWireup.cs', 'Config/HighwayMappings.cs', 'Config/HighwayDataContext.cs', 'Config/HighwayContextConfiguration.cs', 'Config/HighwayDatabaseInitializer.cs', 'Installers/HighwayDataInstaller.cs' ]
 		files.each do |file|
-			convert_to_pp basePath, file, 'build_data/content/'
+			convert_to_pp SRC_DIR, file, 'build_data/content/'
 		end
+		FileUtils.cp(SOLUTION_DIR + '/nuspec/Highway.Onramp.MVC.Data.nuspec', RAKE_DIR + '/build_data/Highway.Onramp.MVC.Data.nuspec')
+		sh SOLUTION_DIR + '/.nuget/NuGet.exe pack ' + RAKE_DIR + '/build_data/Highway.Onramp.MVC.Data.nuspec -o output'
 	end
 
 	task :logging do
 		create 'build_logging/'
-		basePath = 'src/Templates.MVC/'
 		files = [ 'App_Start/LoggerAnnouncementsWireup.cs', 'BaseTypes/BaseLoggingController.cs', 'Filters/ExceptionLoggingFilter.cs', 'Installers/LoggingInstaller.cs', 'NLog.config'  ]
 		files.each do |file|
-			convert_to_pp basePath, file, 'build_logging/content/'
+			convert_to_pp SRC_DIR, file, 'build_logging/content/'
 		end
+		FileUtils.cp(SOLUTION_DIR + '/nuspec/Highway.Onramp.MVC.Logging.nuspec', RAKE_DIR + '/build_logging/Highway.Onramp.MVC.Logging.nuspec')
+		sh SOLUTION_DIR + '/.nuget/NuGet.exe pack ' + RAKE_DIR + '/build_logging/Highway.Onramp.MVC.Logging.nuspec -o output'
 	end
 	
 	def create(path)
