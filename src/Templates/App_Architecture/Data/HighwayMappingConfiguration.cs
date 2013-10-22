b92b03bfda2f8c8f75fb015589ed7836de035111
Tim Rayburn <tim@timrayburn.net>
2013-10-22 10:56:05 -0500

    Removing Data from the gitignore to pickup new folder

---
+++ src/Templates/App_Architecture/Data/HighwayMappingConfiguration.cs
@@ -0,0 +1,24 @@
+﻿// [[Highway.Onramp.MVC.Data]]
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Highway.Data;
+
+namespace Templates.App_Architecture.Data
+{
+    public class HighwayMappingConfiguration : IMappingConfiguration
+    {
+        public void ConfigureModelBuilder(DbModelBuilder modelBuilder)
+        {
+            // TODO Create Mappings Here!
+            modelBuilder.Entity<DeleteMe>();
+        }
+    }
+
+    // TODO Delete this class once you've created your mappings
+    public class DeleteMe
+    {
+        public int Id { get; set; }
+    }
+}
